88e6e729bc9ba4d456774bf57ac77283d440d5dc
Junghun Kim <junghkim@c7r4s7.42seoul.kr>
2021-05-24 19:50:21 +0900

    cub3D

--- cub3D/utils.c
+++ cub3D/utils.c
@@ -1,18 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   free_utils.c                                       :+:      :+:    :+:   */
+/*   utils.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: junghkim <junghkim@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/23 16:45:30 by junghkim          #+#    #+#             */
-/*   Updated: 2021/05/23 16:45:30 by junghkim         ###   ########.fr       */
+/*   Updated: 2021/05/24 19:47:25 by junghkim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
-void	clean_sprites(t_adata *a, t_sps *sps)
+void	clean_sprites(t_data *a, t_sps *sps)
 {
 	int		i;
 
